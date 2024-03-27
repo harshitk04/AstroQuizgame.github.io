adaccb337313329330e262d8890142635ca30435
harshitk04 <149113598+harshitk04@users.noreply.github.com>
2024-03-27 22:18:05 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions=[
+    {
+        numb:1,
+        question:"Where would you see the Great Red Spot?",
+        answer:"Jupiter",
+        options:[
+            "On a desert in North Africa",
+            "Jupiter",
+            "The Milky Way",
+            "The Sun"
+    ]
+    },
+    {
+        numb:2,
+        question:"Who was the first person to walk on the Moon?",
+        answer:"Neil Armstrong",
+        options:[
+            "Buzz Aldrin",
+            "Yuri Gagarin",
+            "Neil Armstrong",
+            "Brian Cox"            
+        ]
+    },
+    {
+        numb:3,
+        question:"What is a supernova?",
+        answer:"electrified particles enter the Earth's atmosphere",        
+        options:[
+            "electrified particles enter the Earth's atmosphere",
+            "there is a rainbow in the sky",
+            "there is an eclipse of the Moon",        
+            "there are lightning strikes in the sky"
+        ]
+    },
+    {
+        numb:4,
+        question:"The bright star Betelgeuse is in which constellation?",
+        answer:"Orion - the Hunter",
+        options:[
+            "Orion - the Hunter",
+            "Taurus - the Bull",
+            "Leo - the Lion",
+            "Cassiopeia - the Queen"
+        ]
+    },
+    {
+        numb:5,
+        question:"Astronomers believe the Universe began...",
+        answer:"almost 14 billion years ago",
+        options:[
+            "around the time of the dinosaurs",
+            "1000 years ago",
+            "when the Sun and planets formed",
+            "almost 14 billion years ago"
+        ]
+    }
+]
